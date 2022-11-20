2dcbaadaf8b0b0d4247f8d0fc2d893d2c2e2b562
Iana Astafeva <88724832+ianastafeva@users.noreply.github.com>
2022-11-20 14:15:22 +0100

    Update main.py

--- Epigene_labs/software-challenge/backend/main.py
+++ Epigene_labs/software-challenge/backend/main.py
@@ -76,3 +76,20 @@ def read_gene_sets(gene_name: str, db: Session = Depends(get_db)):
 def read_all_genesets_slice(slice_st: int, slice_end:int, db: Session = Depends(get_db)):
     genesets = crud.get_genesets_slice(db, skip=slice_st, limit=slice_end)
     return genesets
+
+# addition nb 5
+'''Function that allows the user to search for genes based in a similarty of input gene name'''
+
+@app.get("/genesets/search/similar-gene-{similarty_threshold}/{gene_name}", response_model=List[schemas.Gene])
+def read_similar_gene(similarty_threshold: float, gene_name: str, db: Session = Depends(get_db)):
+    genes = crud.get_similar_gene(db, gene_name, similarty_threshold)
+    return genes
+
+
+# addition nb 6
+'''Function that allows the user to search for genesset based in a similarty of input geneset title'''
+
+@app.get("/genesets/search/similar-geneset-{similarty_threshold}/{geneset_title}", response_model=List[schemas.Geneset])
+def read_similar_genesets(similarty_threshold: float, geneset_title: str, db: Session = Depends(get_db)):
+    genesets = crud.get_similar_genesets(db, geneset_title, similarty_threshold)
+    return genesets
